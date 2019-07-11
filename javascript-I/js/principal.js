a42c03f38db61a6e8d7c80b4878e2037438bb06c
Haysa Rodrigues <hjsilva@thoughtworks.com>
2019-07-11 13:40:13 -0300

    second part java script course

--- javascript-I/js/principal.js
+++ javascript-I/js/principal.js
@@ -1,2 +1,34 @@
-var title = document.querySelector('.titulo')
-title.textContent = "Aparecida Nutricionista"
+var pacients = document.querySelectorAll(".patient");
+
+for (var i = 0; i < pacients.length; i++) {
+
+    var patient = pacients[i];
+
+    var tdPeso = patient.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = patient.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = patient.querySelector(".info-imc");
+
+    var pesoIsValid = true;
+    var alturaIsValid = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoIsValid = false;
+        tdImc.textContent = "Peso inválido";
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaIsValid = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+  if (alturaIsValid && pesoIsValid) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
